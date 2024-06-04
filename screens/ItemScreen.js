56610af407ac478ea760cd575e7f5ea96f1ba158
inci Bakımcı <incibakimci22@gmail.com>
2024-06-04 14:03:39 +0300

    güncellendi
    güncellendi

--- screens/ItemScreen.js
+++ screens/ItemScreen.js
@@ -1,6 +1,14 @@
-import { View, Text, SafeAreaView, ScrollView, Image } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const ItemScreen = ({ route }) => {
   const navigation = useNavigation();
@@ -13,7 +21,7 @@ const ItemScreen = ({ route }) => {
   }, []);
 
   return (
-    <SafeAreaView className="flex-1 bg-white relative">
+    <SafeAreaView className="flex-1 bg-white relative mt-5">
       <ScrollView className="flex-1 px-4 py-6">
         <View className="relative bg-white shadow-lg">
           <Image
@@ -24,6 +32,31 @@ const ItemScreen = ({ route }) => {
             }}
             className="w-full h-72 rounded-2xl object-cover"
           />
+
+          <View className="absolute flex-row inset-x-0 top-5 justify-between px-6">
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Discover")}
+              className="w-10 h-10 rounded-md items-center justify-center bg-white"
+            >
+              <FontAwesome5 name="chevron-left" size={24} color="#06B2BE" />
+            </TouchableOpacity>
+
+            <TouchableOpacity className="w-10 h-10 rounded-md items-center justify-center bg-[#06B2BE]">
+              <FontAwesome5 name="heartbeat" size={24} color="#fff" />
+            </TouchableOpacity>
+          </View>
+
+          <View className="absolute flex-row inset-x-0 bottom-5 justify-between px-6">
+            <View className="flex-row space-x-2 items-center">
+              <Text className="text-[12px] font-bold text-gray-100">
+                {data?.price_level}
+              </Text>
+
+              <Text className="text-[32px] font-bold text-gray-100">
+                {data?.price}
+              </Text>
+            </View>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
